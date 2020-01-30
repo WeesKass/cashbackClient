68fb8e1ad7bbe296d45c997b0b8bc746db26a74c
Бакай Кыдырбек уулу <bakaiks312@gmail.com>
2020-01-30 20:40:40 +0600

    Spring Application for CashBack Client Service

---
+++ src/main/java/kg/nurtelecom/cashbackclient/CashbackclientApplication.java
@@ -0,0 +1,13 @@
+package kg.nurtelecom.cashbackclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbackclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashbackclientApplication.class, args);
+    }
+
+}
