6be6b050f96856aef6e6f915dddbe2eb509a6106
tkasenov <timur.kasenov@iaau.edu.kg>
2020-03-19 13:34:15 +0600

    history page

--- src/main/java/kg/nurtelecom/cashbackclient/controller/HistoryController.java
+++ src/main/java/kg/nurtelecom/cashbackclient/controller/HistoryController.java
@@ -1,13 +1,19 @@
 package kg.nurtelecom.cashbackclient.controller;
 
+import kg.nurtelecom.cashbackclient.service.BalanceHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HistoryController {
+    @Autowired
+    BalanceHistoryService balanceHistoryService;
+
     @GetMapping(value = "/historyPage")
     public String historyPage(Model model){
+        model.addAttribute("historyList", balanceHistoryService.getAllHistory(1L));
         return "historyPage";
     }
 }
---
+++ src/main/java/kg/nurtelecom/cashbackclient/service/BalanceHistoryService.java
@@ -0,0 +1,38 @@
+package kg.nurtelecom.cashbackclient.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kg.nurtelecom.cashbackclient.model.HistoryModel;
+import kg.nurtelecom.cashbackclient.model.OrganizationModel;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.*;
+
+@Service
+public class BalanceHistoryService {
+    private final RestTemplate restTemplate;
+
+    BalanceHistoryService (RestTemplateBuilder restTemplateBuilder) {
+        restTemplate = restTemplateBuilder.build();
+    }
+
+    public List<HistoryModel> getAllHistory(Long id){
+        String url = "http://localhost:8080/api/balanceHistory/client/{id}";
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json =  restTemplate.getForObject(url, String.class, 1);
+        System.out.println(json);
+        List<HistoryModel> result = new ArrayList<>();
+
+        try {
+            result = Arrays.asList(mapper.readValue(json, HistoryModel[].class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+}
