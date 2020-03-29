a9a26bacdfb7808d7ee5393b5acdd39e60d77efa
vladbi-97@mail.ru <Leha3904481255>
2020-03-29 22:22:26 +0600

    made a design

--- src/main/java/kg/nurtelecom/cashbackclient/controller/SearchController.java
+++ src/main/java/kg/nurtelecom/cashbackclient/controller/SearchController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class SearchController {
     @GetMapping(value = "/searchPage")
-    public String searchPage(Model model){
+    public String searchPage(Model model, String search){
+        model.addAttribute("searchResult", search);
         return "searchPage";
     }
 
