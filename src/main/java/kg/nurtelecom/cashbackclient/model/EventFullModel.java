c7b2c12e4bc7a976e494391f398b312c00091962
max <maxims884@gmail.com>
2020-04-17 21:43:57 +0600

    Notification Page

--- src/main/java/kg/nurtelecom/cashbackclient/model/EventFullModel.java
+++ src/main/java/kg/nurtelecom/cashbackclient/model/EventFullModel.java
@@ -14,6 +14,7 @@ import java.util.Date;
 public class EventFullModel {
     private Long id;
     private String name;
+    private String image;
     private Date dateFrom;
     private Date dateTo;
     private String description;
